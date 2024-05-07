a043e7ff54ab15c16f9b8760072e50e733c8fe0d
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2024-05-07 17:13:03 +0100

    Refactor Projects component and update project data

--- src/components/projects.data.jsx
+++ src/components/projects.data.jsx
@@ -1,42 +1,65 @@
-import cpc from '../assets/cpcplatform.png';
-import enicarLearning from '../assets/enicarLearning.png';
-import donkeykong from '../assets/donkeykong.png';
-import streamVibe from '../assets/streamvibe.png';
+import cpc from '../assets/projects/cpcplatform.png';
+import enicarLearning from '../assets/projects/enicarLearning.png';
+import donkeykong from '../assets/projects/donkeykong.png';
+import streamVibe from '../assets/projects/streamvibe.png';
+import cpcPlatformBlog from './blogs/cpcPlatformBlog';
 
-let data =  [
+
+
+let projects =  [
     {
         id: 1,
         title: 'CPC Platform',
         description: 'A competitive programming platform for my club',
-        image: cpc,
-        github: 'https://github.com',
-        demo: 'https://demo.com'
+        thumbnail: cpc,
+        carousel : [
+            {
+                type : 'image',
+                media : cpc,
+            },
+            {
+                type : 'image',
+                media : '/src/assets/projects/cpc1.png',
+            },
+            {   
+                type : 'image',
+                media : '/src/assets/projects/cpc2.png',
+            }
+        ],
+        github : "https://github.com/YassineBenAbdelaziz/CPC-Platform",
+        demo : undefined,
+        blog : cpcPlatformBlog
     },
     {
         id: 2,
         title: 'Enicar Learning',
         description: 'Elearning platform for my school',
-        image: enicarLearning,
-        github: 'https://github.com',
-        demo: 'https://demo.com'
+        thumbnail: enicarLearning,
     },
     {
         id: 3,
         title: 'Donkey Kong',
         description: '2D Donkey Kong built with Java',
-        image: donkeykong,
-        github: 'https://github.com',
-        demo: 'https://demo.com'
+        thumbnail: donkeykong,
+        carousel : [
+            {
+                type : 'image',
+                media : donkeykong,
+            },
+            {
+                type : 'video',
+                media : '/src/assets/projects/donkeykong.mp4',
+            }
+        ],
+        github : "https://github.com/jebalirami7/Classic-Donkey-Kong",
+        demo : undefined,
     },
     {
         id: 4,
-        title: 'SteamVibe',
+        title: 'StreamVibe',
         description: 'Movies and Tv Series Streaming website',
-        image: streamVibe,
-        github: 'https://github.com',
-        demo: 'https://demo.com'
-    }
-
+        thumbnail: streamVibe,
+    },
 ];
 
-export default data ;
+export default projects ;
