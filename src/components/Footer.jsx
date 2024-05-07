9a19bf9e7f547017b57896e321d427128ff3568f
Yassine Ben Abdelaziz <84929030+YassineBenAbdelaziz@users.noreply.github.com>
2024-05-07 17:13:47 +0100

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <p>© All rights reserved - 2024</p>
+        </footer>
+    );
+};
+
+export default Footer;
